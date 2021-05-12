d574c325b214fa824b95a5031c196f44312ee1a3
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-05-12 15:29:12 +0600

    Step-1
    added components,,,,Search.js ,,,,modified Index.css

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search()
+{
+    return(
+        <section className="searchbox-wrap">
+           <input type="text" placeholder="Search for a Movie...." className="searchbox"/>
+        </section>
+    )
+}
+
+export default Search
